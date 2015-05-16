86646a8b052142bb66398f7f4f1126580cea7521
尼奥 <1988wangxiao@gmail.com>
2015-05-16 23:18:40 +0800

    服务端增加实时通信逻辑。

--- app/scripts/server.js
+++ app/scripts/server.js
@@ -1,70 +1,132 @@
-AV.initialize("7f69g7feabv7lts11vtjlu13q5ngrtab0hs426535nh2eotl", 
-    "plvj3ewitm5k3wamysd593tp2l9f61bp6o4ma6vtxp9j7h5y");
+void function() {
 
-var player = function() {
-    var Player = AV.Object.extend('Player');
-    var p = new Player();
-    var attr = {
-        name: '',
-        level: 1,
-        score: 0
-    };
+    var appId = '7f69g7feabv7lts11vtjlu13q5ngrtab0hs426535nh2eotl';
+    var appKey = 'plvj3ewitm5k3wamysd593tp2l9f61bp6o4ma6vtxp9j7h5y';
+    var roomId = '55571ad2e4b032516125f0e4';
+    var maps = ['a', 'b', 'c'];
 
-    function listAll(callback) {
-        var query = new AV.Query(Player);
-        query.find({
-            success: function(results) {
-                callback(results);
+    AV.initialize(appId, appKey);
+
+    window.player = function() {
+
+        // 用户数据
+        var attr = {
+            name: '',
+            score: []
+        };
+
+        var eventCenter = $({});
+        var room;
+
+        // 游戏状态  0 准备开始 1 正在玩 2 准备结束
+        var status = 0;
+
+        var otherData = {};
+
+        function isEnd(myData, otherData) {
+            if (status === 2) {
+                eventCenter.trigger('end', {
+                    results: [myData, otherData]
+                });
+                status = 0;
             }
-        });
-    }
-
-    return {
-        attr: attr,
-        login: function(name, callback) {
-            var me = this;
-            me.attr.name = name;
-            p.save(me.attr, {
-                success: function(object) {
-                    callback && callback();
-                }
-            });
-        },
-        match: function(callback) {
-            var me = this;
-            var max = 50;
-            listAll(function(results) {
-                f(results);
-            });
-            var f = function(results) {
-                var i = Math.floor(Math.random() * (results.length - 1));
-                if (results[i].attributes.name !== attr.name && results[i].attributes.score) {
-                    callback(results[i].attributes);
-                } else if (max) {
-                    max --;
-                    f(results);
-                } else {
-                    callback({});
-                }
-            };
-        },
-        set: function(data, callback) {
-            p.save(data, {
-                success: function(object) {
-                    callback && callback();
-                }
-            });
         }
-    }
-};
 
-// var t = player();
+        function isStart(otherData) {
+            if (status === 0) {
+                eventCenter.trigger('start', {
+                    maps: maps,
+                    user: otherData
+                });
+                status = 1;
+            }
+        }
+
+        return {
+            attr: attr,
+            login: function(name, callback) {
+                var me = this;
+                me.attr.name = name;
+
+                // 创建实时通信实例
+                var rt = AV.realtime({
+                    appId: appId,
+                    clientId: name,
+                    secure: false
+                });
+                rt.on('open', function() {
+                    rt.room(roomId, function(obj) {
+                        if (obj) {
+                            room = obj;
+                            room.join();
+                            room.send({
+                                status: 'online',
+                                user: attr
+                            }, function() {
+                                callback && callback();
+                            });
+                        } else {
+                            alert('服务器的聊天数据被删！');
+                        }
+                    });
+
+                    rt.on('message', function(data) {
+                        otherData = data.msg.user;
+                        switch(data.msg.status) {
+                            case 'online':
+                                room.send({
+                                    status: 'start',
+                                    user: attr
+                                });
+                                isStart(otherData);
+                            break;
+                            case 'start':
+                                isStart(otherData);
+                            break;
+                            case 'end':
+                                isEnd(me.attr, otherData);
+                                status = 2;
+                            break;
+                        }
+                    });
+
+                });
+                return this;
+            },
+            end: function(myData) {
+                var me = this;
+                isEnd(me.attr, otherData);
+                status = 2;
+                for (var k in myData) {
+                    me.attr[k] = myData[k];
+                }
+                room.send({
+                    status: 'end',
+                    user: me.attr
+                });
+                return this;
+            },
+            on: function(eventName, fun) {
+                eventCenter.on(eventName, fun);
+                return this;
+            }
+        };
+    };
+}();
+
+// var t = window.player();
 // t.login('aaa', function() {
-//     t.match(function(data) {
-//         console.log(data);
-//         t.set({
-//             level: 1,
-//             score: 60
-//         });
-//     });
+//     console.log('123');
+// });
+
+// // t.end({
+// //     score: [30, 20, 30]
+// // });
+
+// t.on('start', function(event, data) {
+//     console.log('start', data);
+// });
+
+// t.on('end', function(event, data) {
+//     console.log('end', data);
 // });
