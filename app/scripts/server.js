1beccdf9c33ad065d3f6572daac41ef11d3d053c
尼奥 <1988wangxiao@gmail.com>
2015-05-16 16:36:23 +0800

    接口服务器端除重

--- app/scripts/server.js
+++ app/scripts/server.js
@@ -4,6 +4,11 @@ AV.initialize("7f69g7feabv7lts11vtjlu13q5ngrtab0hs426535nh2eotl",
 var player = function() {
     var Player = AV.Object.extend('Player');
     var p = new Player();
+    var attr = {
+        name: '',
+        level: 1,
+        score: 0
+    };
 
     function listAll(callback) {
         var query = new AV.Query(Player);
@@ -15,20 +20,33 @@ var player = function() {
     }
 
     return {
+        attr: attr,
         login: function(name, callback) {
-            p.save({
-                name: name
-            }, {
+            var me = this;
+            attr.name = name;
+            p.save(me.attr, {
                 success: function(object) {
                     callback && callback();
                 }
             });
         },
         match: function(callback) {
+            var me = this;
+            var max = 5;
             listAll(function(results) {
-                var i = Math.floor(Math.random() * (results.length - 1));
-                callback(results[i].attributes);
+                f(results);
             });
+            var f = function(results) {
+                var i = Math.floor(Math.random() * (results.length - 1));
+                if (results[i].attributes.name !== attr.name && results[i].attributes.score) {
+                    callback(results[i].attributes);
+                } else if (max) {
+                    max --;
+                    f(results);
+                } else {
+                    callback({});
+                }
+            };
         },
         set: function(data, callback) {
             p.save(data, {
@@ -39,3 +57,14 @@ var player = function() {
         }
     }
 };
+
+// var t = player();
+// t.login('wangxiao', function() {
+//     t.match(function(data) {
+//         console.log(data);
+//         t.set({
+//             level: 1,
+//             score: 60
+//         });
+//     });
+// });
